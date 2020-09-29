7a3bfd6e3ce999e93c4591e2d35c5a3169619582
ngls17 <yah@jooble.com>
2020-09-29 19:15:04 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import json
+
+from remover.remover import Remover
+
+if __name__ == '__main__':
+    with open('config.json', 'r', encoding='utf-8') as f:
+        config = json.load(f)
+
+    if "root" not in config:
+        raise KeyError('root is required argument')
+
+    remover_instance = Remover(config["root"],
+                               delete_dir=config.get("delete_dir"),
+                               file_ext=config.get("file_ext"),
+                               parent_ext=config.get("parent_ext"))
+    remover_instance.delete_parentless_file()
+
+    print("Success!")
---
+++ remover/remover.py
@@ -0,0 +1,25 @@
+import os
+
+
+class Remover:
+
+    def __init__(self, directory, delete_dir=False, file_ext='.bak', parent_ext='.doc'):
+        self.directory = directory
+        self.delete_dir = delete_dir
+        self.file_ext = file_ext
+        self.parent_ext = parent_ext
+
+    def delete_parentless_file(self):
+        for root, dirs, files in os.walk(self.directory, topdown=False):
+            for file in files:
+                filename, file_extension = os.path.splitext(file)
+                if file_extension == self.file_ext and filename + self.parent_ext not in files:
+                    os.remove(os.path.join(root, file))
+
+            if self.delete_dir:
+                self.delete_empty_directory(root)
+
+    @staticmethod
+    def delete_empty_directory(directory):
+        if len(os.listdir(directory)) == 0:
+            os.rmdir(directory)
